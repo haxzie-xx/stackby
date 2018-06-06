2af51683cee4c280ea7137db29d315115e4deb51
Musthaq Ahamad <musthu.gm@gmail.com>
2018-06-07 00:33:19 +0530

    Added stackby type

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# stackby
+# StackBy [under development]
 Simple python CLI to stack files in a directory based on type, date and extension
+
+### Installation
+```
+pip3 install -r requirements.txt
+```
+### Usage
+```
+#stack by file extension
+$ python3 stackby.py ext <DIRECTORY_PATH>
+```
---
+++ stackby.py
@@ -0,0 +1,53 @@
+import fire, re
+from os import listdir, getcwd, makedirs, rename
+from os.path import isfile, isdir, join
+
+class StackBy:
+  """ Fire Class for StackBy Operations """
+  def __init__(self):
+    print("Welcome to stackby")
+
+  """ Method to return all files in the given directory """
+  def getFiles(self, dir):
+    #get all the contents of the dir
+    #add valid file to the array
+    files = [filename for filename in listdir(dir) if isfile(join(dir, filename))]
+    return files
+
+  """ Function to stack the given directory based on extensions"""
+  def ext(self, dir = getcwd()):
+    #if the given directory is invalid, return
+    if not isdir(dir):
+      print('Invalid Directory')
+      return
+
+    #get all the filenames and validate based on extension
+    #this will avoid dotfiles and files with no extensions
+    for filename in self.getFiles(dir):
+      result = re.match(r'^[A-Za-z0-9_\-\\()\s,\.\'@\[\]]+\.([a-zA-Z0-9]+)$', filename)
+      if not result:
+        continue
+      
+      #get the file extension
+      extention = result.groups()[0]
+      #generate the new directory of the file
+      file_dir = join(dir, extention)
+      #if the directory doesn't exist,
+      if not isdir(file_dir):
+        print("Creating Directory: ", file_dir)
+        #create the new directory
+        makedirs(file_dir)
+      #finally, move the file to the new extension directory
+      print("Moving: ",filename," -> ",extention,"/",filename)
+      rename(join(dir, filename), join(file_dir, filename))
+    
+    """ Function to stack files based on type of predetermined filetypes """
+    def type(self, dir = getcwd()):
+      print("Not yet implemented")
+
+    """ Function to stack files based on created date """
+    def created(self, dir = getcwd()):
+      print("Not yet implemented")
+      
+if __name__ == '__main__':
+  fire.Fire(StackBy)
